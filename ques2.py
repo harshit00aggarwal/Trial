a51634a231dc2a0479b9418bebdef03c662215bc
harshit00aggarwal <80538830+harshit00aggarwal@users.noreply.github.com>
2022-04-08 13:31:20 +0530

    Update ques2.py

--- ques2.py
+++ ques2.py
@@ -7,22 +7,16 @@ while(flag != 1):
     a.append(pasw)
     flag=int(input("To end enter 1 else to add more enter 0: "))
 for pwd in a:
-    valid=True
     while(True):
         if len(pwd)<8:
-            valid = False
             break
         elif not re.search("[a-z]",pwd):
-            valid=False
             break
         elif not re.search("[A-Z]",pwd):
-            valid = False
             break
         elif not re.search("[0-9]",pwd):
-            valid = False
             break
         elif not re.search("[@,#,$]",pwd):
-            valid = False
             break
         else:
             b.append(pwd)
